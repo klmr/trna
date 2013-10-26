e609ecef70aa7dca0c3606abdec3b7be7b2c4135
Konrad Rudolph <konrad.rudolph@gmail.com>
2013-10-26 20:45:19 +0100

    Simplified code for silenced library loading
    Using the power of namespaces

--- common/scripts/basic.R
+++ common/scripts/basic.R
@@ -1,17 +1,13 @@
-# Silence package loading – ensure it’s only run once:
+# Silence package loading
 
-if (! exists('oldLibrary')) {
-    oldLibrary <- library
-    library <- function (package)
-        suppressPackageStartupMessages(
-            oldLibrary(as.character(substitute(package)),
-                       verbose = FALSE, character.only = TRUE))
+library <- function (package)
+    suppressPackageStartupMessages(
+        base::library(as.character(substitute(package)),
+                      verbose = FALSE, character.only = TRUE))
 
-    oldRequire <- require
-    require <- function (package)
-        suppressPackageStartupMessages(
-            oldRequire(as.character(substitute(package)), character.only = TRUE))
-}
+require <- function (package)
+    suppressPackageStartupMessages(
+       base::require(as.character(substitute(package)), character.only = TRUE))
 
 # Qualified path to make this usable everywhere.
 source('../common/scripts/definitions.R', chdir = TRUE)
