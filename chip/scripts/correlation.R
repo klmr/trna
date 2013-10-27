3d0b30138b67b8c8a3bcef0def2a21128d89ae97
Konrad Rudolph <konrad.rudolph@gmail.com>
2013-10-27 20:03:17 +0000

    Correlation plots for ChIP data

--- chip/scripts/correlation.R
+++ chip/scripts/correlation.R
@@ -6,33 +6,9 @@ local({
     source('scripts/correlation.R')
 })
 
-prepareForPairsPlot <- function (df) {
-    mat <- list(brain = df[, grep('brain', colnames(df))],
-                liver = df[, grep('liver', colnames(df))])
-    newcols <- gsub('brain_', '', colnames(mat[[1]]))
-    colnames(mat[[1]]) <- newcols
-    colnames(mat[[2]]) <- newcols
-    data.frame(do.call(rbind, mat[tissues]),
-               type = rep(tissues, each = nrow(df)))
-}
-
 trnaGroupFamilyAndType <- function () {
-    conditions <- unique(trnaMapping$Condition)
-    trnaStageCount <<- prepareForPairsPlot(trnaNormDataCond)
-
-    trnaByAcceptor <- aggregate(trnaNormDataCond,
-                                by = list(Condition = trnaAnnotation[rownames(trnaNormDataCond), 'Acceptor']),
-                                mean)
-    rownames(trnaByAcceptor) <- trnaByAcceptor$Condition
-    trnaByAcceptor$Condition <- NULL
-    trnaByAcceptor <<- prepareForPairsPlot(trnaByAcceptor)
-
-    trnaByType <- aggregate(trnaNormDataCond,
-                            by = list(Condition = trnaAnnotation[rownames(trnaNormDataCond), 'Type']),
-                            mean)
-    rownames(trnaByType) <- trnaByType$Condition
-    trnaByType$Condition <- NULL
-    trnaByType <<- prepareForPairsPlot(trnaByType)
+    trnaByAcceptor <<- groupby(trnaNormDataCond, trnaAnnotation$Acceptor, mean)
+    trnaByType <<- groupby(trnaNormDataCond, trnaAnnotation$Type, mean)
 }
 
 relativeData <- function (data) {
