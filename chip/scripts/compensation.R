7799b51515ac51ecf210e5bf64b99a938daf0862
Konrad Rudolph <konrad.rudolph@gmail.com>
2013-12-16 16:10:28 +0000

    Final cleanup to move everything into functions
    All relevant code is encapsulated now, and invoked for both tissues. The
    relevant plots are created, and diagnostic messages generated.

--- chip/scripts/compensation.R
+++ chip/scripts/compensation.R
@@ -34,10 +34,6 @@ plotCorrelation <- function (data, ...)
 plotAcceptorCorrelation <- function (acceptor)
     plotCorrelation(trnaAcceptorCor[[acceptor]], main = paste('Acceptor', acceptor))
 
-plotBackgroundCorrelation <- function (acceptor)
-    plotCorrelation(trnaBackgroundCor[[acceptor]],
-                    main = paste('Acceptor background', acceptor))
-
 scrambledCor <- function (x, method = 'pearson',
                           permutation = sample.int(nrow(x))) {
     result <- matrix(NA, nrow = ncol(x), ncol = ncol(x))
@@ -54,90 +50,102 @@ scrambledCor <- function (x, method = 'pearson',
     result
 }
 
+acceptorData <- function (acceptor, tissue)
+    let(geneIds = rownames(subset(trnaAnnotation, Acceptor == acceptor)),
+        cols = tissueCols(tissue),
+        trnaNormDataCond[geneIds, cols])
+
+findGeneClusters <- function (data) {
+    if (nrow(data) < 2)
+        return()
+
+    # pvclust can run into various errors. Nothing to be done.
+    pvclusters <- silent(try(suppressWarnings(pvclust(t(data), nboot = 100)),
+                         silent = TRUE))
+
+    if (! is(pvclusters, 'try-error'))
+        pvpick(pvclusters)$clusters
+}
+
+clusterMeans <- function (acceptor, clust, data) {
+    # Only consider cases with two clearly distinct clusters.
+    if (length(clust) != 2) {
+        message('Skipping ', acceptor, ' because it has ',
+                length(clust), ' clusters')
+        return()
+    }
+    do.call(rbind, lapply(clust, function (c) colMeans(data[c, ])))
+}
+
+plotTestVis <- function (tissue, totalBackground, observations, test) {
+    on.exit(dev.off())
+    pdf(sprintf('plots/compensation/%s.pdf', tissue))
+    hist(totalBackground, breaks = 25, col = 'grey', border = 'grey',
+         main = 'Background distribution of correlations',
+         xlab = 'Correlation coefficient',
+         ylab = 'Frequency of correlation coefficient')
+    invisible(map(fun(x = abline(v = x, col = colors[1])), observations))
+    par(usr = c(0, 1, 0, 1))
+    text(1, 0.9, 'Observed\ncorrelations', col = colors[1], pos = 2)
+    text(1, 0.1, bquote(italic(p) == .(sprintf('%0.3f', test$p.value))),
+         col = colors[1], pos = 2)
+}
+
+plotCorrelationDistribution <- function (tissue, trnaAcceptorCor, observations) {
+    on.exit(dev.off())
+    pdf(sprintf('plots/compensation/%s-correlations.pdf', tissue))
+    negCorAcceptors <- names(which(observations < -0.5))
+    correlations <- map(fun(n = trnaAcceptorCor[[n]][upper.tri(trnaAcceptorCor[[n]])]),
+                        negCorAcceptors) %|% unlist
+
+    hist(correlations, breaks = 25, freq = FALSE, col = 'grey', border = 'grey')
+    lines(density(correlations), lwd = 2, col = colors[1])
+}
+
+compensationAnalysis <- function (tissue) {
+    #' @TODO Is rank correlation really appropriate here?
+    corMethod <- 'spearman'
+    acceptors <- unique(trnaAnnotation$Acceptor)
+    data <- setNames(map(acceptorData, acceptors, tissue), acceptors)
+    clust <- map(findGeneClusters, data)
+    clusters <- map(clusterMeans, names(clust), clust, data)
+    clusters <- filter(neg(is.null), clusters)
+    message('Using ', length(clusters), ' out of ', length(clust), ' isoacceptor families')
+
+    observations <- map(fun(c = cor(c[1, ], c[2, ], method = corMethod)), clusters)
+    observations <- unlist(observations)
+    permutations <- uperm(ncol(clusters[[1]]))
+    background <- map(fun(c = apply(permutations, ROWS,
+                                    fun(p = cor(c[1, ], c[2, p],
+                                                method = corMethod)))), clusters)
+    totalBackground <- do.call(c, background)
+    test <- ks.test(observations, totalBackground, alternative = 'greater')
+
+    plotTestVis(tissue, totalBackground, observations, test)
+
+    trnaAcceptorCor <- sapply(as.character(unique(trnaAnnotation$Acceptor)),
+                              isoacceptorCorrelations, tissue)
+
+    plotCorrelationDistribution(tissue, trnaAcceptorCor, observations)
+
+    structure(list(clusterSizes = table(unlist(map(length, clust))),
+                   p.value = test$p.value),
+              class = 'compensation')
+}
+
+print.compensation <- function (x, ...) {
+    cat('Frequencies of cluster sizes for analysis:')
+    print(x$clusterSizes)
+    cat(sprintf('\np-value = %0.5f (ks.test)\n', x$p.value))
+    x
+}
+
 if (! interactive()) {
     trnaLoadData()
     trnaSetupCountDataSet()
     trnaNormalizeData()
 
-    #' @TODO Use as starting point to refactor in to function
-    tissue <- 'liver'
-    trnaAcceptorCor <- sapply(as.character(unique(trnaAnnotation$Acceptor)),
-                              isoacceptorCorrelations, tissue)
-
-    trnaBackgroundCor <- sapply(as.character(unique(trnaAnnotation$Acceptor)),
-                                createRandomBackground, tissue)
-
-    acceptorData <- function (acceptor)
-        let(geneIds = rownames(subset(trnaAnnotation, Acceptor == acceptor)),
-            cols = tissueCols(tissue),
-            trnaNormDataCond[geneIds, cols])
-
-    findGeneClusters <- function (data) {
-        if (nrow(data) < 2)
-            return()
-
-        # pvclust can run into various errors. Nothing to be done.
-        pvclusters <- silent(try(suppressWarnings(pvclust(t(data), nboot = 100))))
-
-        if (! is(pvclusters, 'try-error'))
-            pvpick(pvclusters)$clusters
-    }
-
-    clusterMeans <- function (clust, data) {
-        # Only consider cases with two clearly distinct clusters.
-        if (length(clust) != 2) {
-            warning('Skipping ', acceptor, ' because it has ',
-                    length(clust), ' clusters', call. = FALSE)
-            return()
-        }
-        do.call(rbind, lapply(clust, function (c) colMeans(data[c, ])))
-    }
-
-    #' @TODO Is rank correlation really appropriate here?
-    corMethod <- 'spearman'
-
-    set.seed(123)
-
-    acceptors <- unique(trnaAnnotation$Acceptor)
-    data <- setNames(map(acceptorData, acceptors), acceptors)
-    clust <- map(findGeneClusters, data)
-    clusters <- map(clusterMeans, clust, data)
-    clusters <- filter(neg(is.null), clusters)
-    observations <- map(fun(c = cor(c[1, ], c[2, ], method = corMethod)), clusters)
-    observations <- unlist(observations)
-    background <- map(fun(c = apply(permutations, ROWS,
-                                    fun(p = cor(c[1, ], c[2, p],
-                                                method = corMethod)))),
-                      clusters)
-    totalBackground <- do.call(c, background)
-    test <- ks.test(observations, totalBackground, alternative = 'greater')
-
-    plotTestVis <- function (tissue, totalBackground, observations, test) {
-        on.exit(dev.off())
-        pdf(sprintf('plots/compensation/%s.pdf', tissue))
-        hist(totalBackground, breaks = 25, col = 'grey', border = 'grey',
-             main = 'Background distribution of correlations',
-             xlab = 'Correlation coefficient',
-             ylab = 'Frequency of correlation coefficient')
-        invisible(map(fun(x = abline(v = x, col = colors[1])), observations))
-        par(usr = c(0, 1, 0, 1))
-        text(1, 0.9, 'Observed\ncorrelations', col = colors[1], pos = 2)
-        text(1, 0.1, bquote(italic(p) == .(sprintf('%0.3f', test$p.value))),
-             col = colors[1], pos = 2)
-    }
-
-    plotCorrelationDistribution <- function (tissue, trnaAcceptorCor, observations) {
-        on.exit(dev.off())
-        pdf(sprintf('plots/compensation/%s-correlations.pdf', tissue))
-        negCorAcceptors <- names(which(observations < -0.5))
-        correlations <- map(fun(n = trnaAcceptorCor[[n]][upper.tri(trnaAcceptorCor[[n]])]),
-                            negCorAcceptors) %|% unlist
-
-        hist(correlations, breaks = 25, freq = FALSE, col = 'grey', border = 'grey')
-        lines(density(correlations), lwd = 2, col = colors[1])
-    }
-
     mkdir('plots/compensation')
-    plotTestVis(tissue, totalBackground, observations, test)
-    plotCorrelationDistribution(tissue, trnaAcceptorCor, observations)
+    set.seed(123)
+    map(compensationAnalysis, tissues)
 }
