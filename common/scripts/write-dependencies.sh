359f57aca68eaf9499e92af400326b48f7417571
Konrad Rudolph <konrad.rudolph@gmail.com>
2013-12-20 20:09:24 +0000

    Update dependencies list

--- common/scripts/write-dependencies.sh
+++ common/scripts/write-dependencies.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")/../.."
 grep -hE '\b(require|library)\b' {common,chip,rna}/scripts/*.R | \
     sed '/^[[:space:]]*#/d' | \
-    sed -E 's/.*\(([[:alpha:]]*)\).*/\1/' | \
+    sed -E 's/.*\(([[:alnum:]]*)\).*/\1/' | \
     sort -uf \
     > DEPENDS
