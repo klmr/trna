64ade9a6748c3dc022a0192d53534fb3f1281909
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-01-16 15:21:00 +0000

    Run Meme locally and make function easier to use

--- chip/scripts/prepare-meme-analysis.R
+++ chip/scripts/prepare-meme-analysis.R
@@ -4,40 +4,56 @@ countsForCondition <- function (conditions)
     trnaRawCounts[, colnames(trnaRawCounts) %in%
                     rownames(trnaMapping)[trnaMapping$Condition %in% conditions]]
 
+runMeme <- function (foreground, background, outputPath) {
+    memePath <- '/usr/local/Cellar/meme/4.9.0-p4/bin'
+    memeBin <- file.path(memePath, 'meme')
+    dustBin <- file.path(memePath, 'dust')
+    filterFastaBin <- '../common/scripts/filter-fasta'
+    markovModelBin <- '../common/scripts/markov-model-from-fasta'
+
+    trnaUpstreamFastaFile <- '../common/data/tRNA-upstream-with-ids.fasta'
+
+    mkdir(outputPath)
+
+    system(sprintf('%s < %s %s > %s',
+                   filterFastaBin,
+                   trnaUpstreamFastaFile,
+                   paste(foreground, collapse = ' '),
+                   file.path(outputPath, 'seq.fasta')))
+
+    system(sprintf('bash -c \'%s <(%s < %s %s) > %s\'',
+                   markovModelBin,
+                   filterFastaBin,
+                   trnaUpstreamFastaFile,
+                   paste(background, collapse = ' '),
+                   file.path(outputPath, 'background.mm')))
+
+    system(sprintf('%s %s -dna -oc %s -nostatus -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -bfile %s',
+                   memeBin,
+                   file.path(outputPath, 'seq.fasta'),
+                   file.path(outputPath, 'result'),
+                   file.path(outputPath, 'background.mm')))
+}
+
+runMemeForStages <- function (tissue, a, b) {
+    contrastStages <- paste(tissue, c(a, b), sep = '-')
+    contrast <- paste(contrastStages, collapse = '/')
+    deGenes <- rownames(trnaDeGenes[[contrast]])
+    allGenes <- getExpressedtRNAs(countsForCondition(contrastStages))
+    background <- setdiff(allGenes, deGenes)
+    basePath <- file.path('results/meme', tissue, paste(a, b, sep = '-'))
+    runMeme(deGenes, background, basePath)
+}
+
 if (! interactive()) {
     trnaLoadData()
     trnaPairwiseDiffentialExpression()
-
-    filterFasta <- '../common/scripts/filter-fasta'
-    markovModel <- '../common/scripts/markov-model-from-fasta'
-
-    trnaUpstreamFastaFile <- '../common/data/tRNA-upstream-with-ids.fasta'
 
     # For now, only look at e15.5/* contrasts
 
     for (tissue in tissues) {
         for (stage in stages[-1]) {
-            contrastStages <- paste('liver', c('e15.5', stage), sep = '-')
-            contrast <- paste(contrastStages, collapse = '/')
-            deGenes <- rownames(trnaDeGenes[[contrast]])
-            allGenes <- getExpressedtRNAs(countsForCondition(contrastStages))
-            background <- setdiff(allGenes, deGenes)
-
-            basePath <- file.path('results/meme', tissue, stage)
-            mkdir(basePath)
-
-            system(sprintf('%s < %s %s > %s',
-                           filterFasta,
-                           trnaUpstreamFastaFile,
-                           paste(deGenes, collapse = ' '),
-                           file.path(basePath, 'seq.fasta')))
-
-            system(sprintf('bash -c \'%s <(%s < %s %s) > %s\'',
-                           markovModel,
-                           filterFasta,
-                           trnaUpstreamFastaFile,
-                           paste(background, collapse = ' '),
-                           file.path(basePath, 'background.mm')))
+            runMemeForStages(tissue, 'e15.5', stage)
         }
     }
 }
