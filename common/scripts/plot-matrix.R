f8473a42137d2d072ccfecf908d801307d92d37f
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-02-10 18:12:52 +0000

    Use matrix plot for mRNA expression correlatons
    Using a similar layout as for the DE count matrix plot.

--- common/scripts/plot-matrix.R
+++ common/scripts/plot-matrix.R
@@ -1,9 +1,11 @@
-plotCountMatrix <- function (counts, main) {
-    maxval <- lapply(counts, partial(max, na.rm = TRUE))
+plotCountMatrix <- function (counts, main, format = '%s') {
+    cols <- 100
     gradient <- function (tissue)
         colorRampPalette(c('white', tissueColor[tissue]),
-                         interpolate = 'spline')(maxval[[tissue]] + 1)
+                         interpolate = 'spline')(cols + 1)
     colors <- map(gradient, tissues)
+    range <- map(p(range, na.rm = TRUE), counts)
+    range <- map(.(x = c(x, x[2] - x[1])), range)
 
     w <- nrow(counts[[1]])
     par(mfrow = c(w, w), mar = rep(0, 4), oma = c(1, 1, 4, 10))
@@ -11,15 +13,16 @@ plotCountMatrix <- function (counts, main) {
         for (j in seq(w)) {
             tissue <- if (i < j) 'liver' else 'brain'
             value <- counts[[tissue]][i, j]
+            color <- (value - range[[tissue]][1]) / range[[tissue]][3] * cols
             plot.new()
             rect(par('usr')[1], par('usr')[3], par('usr')[2], par('usr')[4],
-                 col = colors[[tissue]][value + 1], border = NA)
+                 col = colors[[tissue]][color], border = NA)
             if (i == j)
                 text(0.5, 0.5,
                      labels = readable(colnames(counts[[tissue]])[i]),
                      cex = 2, font = 2)
             else
-                text(0.5, 0.5, labels = value, cex = 2)
+                text(0.5, 0.5, labels = sprintf(format, value), cex = 2)
         }
     }
 
@@ -30,9 +33,9 @@ plotCountMatrix <- function (counts, main) {
     xpos = list(liver = 0.25, brain = 0.75)
     for (tissue in names(xpos)) {
         x <- xpos[[tissue]]
-        points(rep(x, maxval[[tissue]]), (1 : maxval[[tissue]]) / maxval[[tissue]],
+        points(rep(x, cols), (1 : cols) / cols,
                col = colors[[tissue]], pch = 15, cex = 3)
-        text(c(x, x), c(0, 1), labels = c(0, maxval[[tissue]]), pos = 4, offset = 1)
+        text(c(x, x), c(0, 1), labels = sprintf(format, range[[tissue]][-3]), pos = 4, offset = 1)
         text(x, 0, labels = sprintf('  %s', capitalize(tissue)), pos = 1, offset = 1)
     }
     par(op)
