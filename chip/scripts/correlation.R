a34e5cdbd754375f17ce0689bd6676c60766472d
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-02-12 10:03:37 +0000

    Recreate tRNA shuffling of expressed genes

--- chip/scripts/correlation.R
+++ chip/scripts/correlation.R
@@ -11,10 +11,7 @@ trnaGroupFamilyAndType <- function () {
     trnaByType <<- groupby(trnaNormDataCond, trnaAnnotation$Type, sum)
 }
 
-resampleAcceptorAbundance <- function () {
-    samples <- 100
-    cds <- trnaGetCountDataSet(trnaUnfilteredRawCounts)
-    data <- trnaMergeReplicates(as.data.frame(counts(cds, normalized = TRUE)))
+trnaDoResample <- function (data) {
     data <- data[, grep('liver', colnames(data))]
     # Reorder columns by stages since they are unordered
     data <- data[, vapply(stages, grep, numeric(1), colnames(data))]
@@ -22,10 +19,19 @@ resampleAcceptorAbundance <- function () {
     sampleColumn <- function (col)
         map(.(. = shuffleRows(data[, col, drop = FALSE])), 1 : samples) %|%
         lp(do.call, cbind)
-    acceptorSampleMatrix <- setNames(map(sampleColumn, colnames(data)),
-                                     colnames(data))
+    setNames(map(sampleColumn, colnames(data)), colnames(data))
+}
 
-    acceptorSampleMatrix <<- acceptorSampleMatrix
+resampleAcceptorAbundance <- function () {
+    samples <- 100
+    cds <- trnaGetCountDataSet(trnaUnfilteredRawCounts)
+    data <- trnaMergeReplicates(as.data.frame(counts(cds, normalized = TRUE)))
+    acceptorSampleMatrix <<- trnaDoResample(data)
+}
+
+resampleExpressedAcceptorAbundance <- function () {
+    sample <- 100
+    expressedAcceptorSampleMatrix <<- trnaDoResample(trnaNormDataCond)
 }
 
 acceptorAbundance <- function (trnaAbundance) {
