4656504287ee60947d7a9f7f938dd8d61328fcb0
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-02-13 11:55:19 +0000

    Add codon bar plots for stable genes

--- rna/scripts/correlation-plots.R
+++ rna/scripts/correlation-plots.R
@@ -65,6 +65,57 @@ plotSpiderWeb <- function (type, codonUsageData, aaUsageData) {
     }
 }
 
+plotRnaCodonDistribution <- function (codonUsageData, name, tissue) {
+    on.exit(dev.off())
+    pdf(sprintf('plots/usage/%s-codons-%s.pdf', name, tissue),
+        width = 8, height = 3.5, family = plotFamily)
+
+    for (aa in aminoAcids$Short) {
+        codons <- rownames(subset(geneticCode, AA == aa))
+        if (length(codons) == 1)
+            next
+
+        mrna <- codonUsageData[codons, grep(tissue, colnames(codonUsageData))]
+        mrna <- relativeData(mrna[codons, ])
+
+        long <- subset(aminoAcids, Short == aa)$Long
+
+        colim <- sapply(stages, p(grep, colnames(mrna)))
+        mrna <- `colnames<-`(mrna[, colim], stages)
+        mrna <- mrna[, ncol(mrna) : 1]
+
+        # We shuffle the order of the colours to avoid giving the misleading
+        # impression of a gradient.
+        grays <- gray.colors(length(colors) - 1)[c(3, 6, 2, 5, 1, 4, 7)]
+
+        local({
+            oldPar <- par(lty = 0, xpd = TRUE)
+            on.exit(par(oldPar))
+            barplot(mrna, horiz = TRUE, col = grays, axes = FALSE,
+                    xlab = 'Proportion of mRNA codon usage',
+                    las = 1, names.arg = readable(colnames(mrna)))
+            axis(1, 0 : 4 / 4, sprintf('%d%%', 0 : 4 * 25), cex.axis = 0.75)
+            legendPos <- mrna[, ncol(mrna)]
+            legendCol <- grays[legendPos != 0]
+            legendPos <- cumsum(legendPos[legendPos != 0])
+            legend <- names(legendPos)
+            legendPos <- c(0, legendPos[-length(legendPos)])
+
+            par(usr = c(0, 1, 0, 1))
+
+            # Adjust label positions so they don’t overlap.
+            legendWidths <- strwidth(legend)
+            if (length(legend) > 1)
+                for (i in 1 : (length(legend) - 1))
+                    if (legendPos[i] + legendWidths[i] > legendPos[i + 1])
+                        legendPos[i + 1] <- legendPos[i] + legendWidths[i]
+
+            text(legendPos, 1, legend, pos = 4, col = legendCol,
+                 cex = 0.75, offset = 0.1)
+        })
+    }
+}
+
 plotCodonBackground <- function () {
     tcolors <- c(gray.colors(5), colors[1])
     lwd <- c(rep(2, 5), 3)
@@ -174,6 +225,8 @@ if (! interactive()) {
     cat('# Generate stable mRNA codon usage plots\n')
     plotSpiderWeb('stable-', stableCodonUsageData, stableAaUsageData)
     plotSpiderWeb('low-', lowCodonUsageData, lowAaUsageData)
+    plotRnaCodonDistribution(stableCodonUsageData, 'stable', 'liver')
+    plotRnaCodonDistribution(lowCodonUsageData, 'low', 'liver')
 
     cat('# Generate background mRNA codon usage plots\n')
     plotCodonBackground()
